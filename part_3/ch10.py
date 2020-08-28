9f6ee6c8f81da496025d0771d970168aa5608e63
henriguy1210 <henri.idrovo@pm.me>
2020-08-27 23:10:46 -0500

    couple REPLs

---
+++ part_3/ch10.py
@@ -0,0 +1,23 @@
+
+while True:
+	reply = input("enter something:")
+	if reply == "stop": break
+	print(reply.upper())
+
+while True:
+	reply = input("enter a number:")
+	if reply == "stop": break
+	replyInt = int(reply)
+	print(replyInt ** 2)
+
+while True:
+	x = input("enter anything!")
+	if x == "stop": break
+	elif not x.isdigit(): print("BAD! " * 3)
+	else: print(int(x) * 2)
+
+
+
+
+
+
